f31c0bcf94350bf35d49e83bdee111ed27a727fe
Milan Sony <milansonyofficial@gmail.com>
2026-03-18 17:34:47 +0530

    feat: task route added

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 
 import authRoutes from "./auth.js";
+import taskRoutes from "./task.js";
 
 const apiV1Router = express.Router();
 
@@ -12,7 +13,10 @@ apiV1Router.get("/", (req, res) => {
     });
 });
 
-// auth routes
+// Auth routes
 apiV1Router.use("/auth", authRoutes);
+
+// Tasks routes
+apiV1Router.use("/tasks", taskRoutes);
 
 export default apiV1Router;
