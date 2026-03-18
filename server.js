ff07f75aff5f1a9f4f02c6cbdfe31f7398ad10fe
Milan Sony <milansonyofficial@gmail.com>
2026-03-18 17:29:50 +0530

    feat: cookie-parser configured

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from 'cookie-parser'
 
 import { baseRoute } from "./routes/index.js";
 import { connectDB } from "./configs/mongoDBConfig.js";
@@ -7,9 +8,15 @@ import { connectDB } from "./configs/mongoDBConfig.js";
 // Creates an express app
 const app = express();
 
+// Connect DB
+connectDB();
+
 // Body parser
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+// config cookie parser
+app.use(cookieParser())
 
 // Config CORS
 app.use(cors({
@@ -19,13 +26,10 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'Authorization'] // Allow necessary headers
 }));
 
-// Connect DB
-connectDB();
-
 // Base route
 app.use("/", baseRoute);
 
 const PORT = Number(process.env.PORT || 5000);
 app.listen(PORT, () => {
-    console.log(`\n🚀 Server listening on port: ${PORT}`);
+  console.log(`\n🚀 Server listening on port: ${PORT}`);
 });
