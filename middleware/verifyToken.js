050023ebbb4621ed76a027e02da4308c4eb5a611
Milan Sony <milansonyofficial@gmail.com>
2026-03-18 17:33:59 +0530

    chore: log added

---
+++ middleware/verifyToken.js
@@ -0,0 +1,38 @@
+import jwt from "jsonwebtoken"
+import { Users } from "../models/userModel.js"
+
+export const verifyToken = async (req, res, next) => {
+
+    const token = req.headers.authorization?.split(' ')[1] // Split the bearer token "bearer efhjkhfjkhjkhsjkhsjkhjgkhkjsh"
+
+    if (!token) {
+        return res.status(401).json({
+            status: 401,
+            message: "Unauthorized"
+        })
+    }
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET_ACCESS_TOKEN)
+
+        const user = await Users.findOne({ _id: decoded.userId }).select("-password")
+
+        console.log("user in verify token: ", user)
+
+        if (!user) {
+            return res.status(400).json({
+                status: 400,
+                message: "User not found"
+            })
+        }
+
+        req.user = user
+
+        next()
+    } catch (error) {
+        return res.status(401).json({
+            status: 401,
+            message: "Invalid token"
+        })
+    }
+}
