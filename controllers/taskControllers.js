94221e5562836de76203113f2644e51f72962c93
Milan Sony <milansonyofficial@gmail.com>
2026-03-20 15:18:39 +0530

    chore: removed status

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -6,7 +6,7 @@ export const createTasks = async (req, res) => {
     try {
         const loggedInUserId = req.user?._id
 
-        const { task, priority, status } = req.body
+        const { task, priority } = req.body
 
         // Check empty input fields
         if (!task) {
@@ -23,18 +23,10 @@ export const createTasks = async (req, res) => {
             })
         }
 
-        if (!status) {
-            return res.status(400).json({
-                status: 400,
-                message: "Status is required"
-            })
-        }
-
         const newTask = new Tasks({
             user_id: loggedInUserId,
             task: task,
             priority: priority,
-            status: status
         })
 
         if (newTask) {
@@ -60,12 +52,20 @@ export const createTasks = async (req, res) => {
     }
 }
 
+// Get all tasks
 export const getTasks = async (req, res) => {
 
     try {
         const loggedInUserId = req.user?._id
 
         const tasks = await Tasks.find({ user_id: loggedInUserId })
+
+        if (!tasks || tasks.length === 0) {
+            return res.status(404).json({
+                status: 404,
+                message: "No tasks found"
+            })
+        }
 
         return res.status(200).json({
             status: 200,
@@ -80,6 +80,85 @@ export const getTasks = async (req, res) => {
             message: "Error fetching the tasks",
             error: error.message
         })
+    }
+}
 
+// Update tasks
+export const updateTasks = async (req, res) => {
+    try {
+        const { taskId, task, priority } = req.body
+
+        if (!taskId) {
+            console.error("Update task Id is missing/not found")
+            return
+        }
+
+        if (!task) {
+            return res.status(400).json({
+                status: 400,
+                message: "Task is required"
+            })
+        }
+
+        if (!priority) {
+            return res.status(400).json({
+                status: 400,
+                message: "priority is required"
+            })
+        }
+
+        const updatedTaskData = {
+            _id: taskId,
+            task: task,
+            priority: priority,
+        }
+
+        console.log("updatedTaskData: ", updatedTaskData)
+
+        const updatedTask = await Tasks.findByIdAndUpdate(taskId, updatedTaskData)
+        if (updatedTask) {
+            return res.status(201).json({
+                status: 201,
+                message: "Task updated successfully",
+            })
+        }
+
+    } catch (error) {
+        console.error("Error editing the tasks", error)
+        return res.status(500).json({
+            status: 500,
+            message: "Error editing the tasks",
+            error: error.message
+        })
+    }
+}
+
+// Delete task
+export const deleteTasks = async (req, res) => {
+    try {
+        const { id: taskId } = req.params
+
+        console.log("receiverId: ", taskId)
+
+        if (!taskId) {
+            console.error("Delete task Id is missing/not found")
+            return
+        }
+
+        const deletedTask = await Tasks.findByIdAndDelete(taskId)
+        if (deletedTask) {
+            return res.status(200).json({
+                status: 200,
+                message: "Task deleted successfully",
+            })
+        }
+
+    } catch (error) {
+        console.error("Error editing the tasks", error)
+        return res.status(500).json({
+            status: 500,
+            message: "Error editing the tasks",
+            error: error.message
+        })
     }
 }
